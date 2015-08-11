9dfeea4d1b8dc4c1afe31a401197d00177f83746
Connor Noble <snowball2k10@hotmail.co.uk>
2015-08-11 15:22:27 +0100

    NewDispatch
    Added a new plugin

--- adoor/plugin/cl_plugin.lua
+++
@@ -1,7 +0,0 @@
-Clockwork.datastream:Hook("DoorPassword", function(data)
-	local entity = data;
-	
-	Derma_StringRequest("Password", "What is the password for this door?", nil, function(text)
-		Clockwork.datastream:Start("DoorPassword", {text, entity});
-	end);
-end);
